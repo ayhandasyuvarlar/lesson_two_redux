1c5244cd0ababe6637e918e850f95d8532effbe3
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-12-19 12:46:01 +0300

    redux_lesson_two

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+const App = () => {
+  const data = useSelector((state) => state.post);
+  console.log(data);
+  return <div>{
+    JSON.stringify(data)
+  }
+  </div>;
+};
 
 export default App;
